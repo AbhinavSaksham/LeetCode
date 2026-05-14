0079f2f4a780095ecfc9060a16151a78e009c4d8
Abhinav Pandey <56484423+AbhinavSaksham@users.noreply.github.com>
2026-05-14 14:12:44 +0530

    Time: 0 ms (100%), Space: 11.3 MB (19.67%) - LeetHub

--- 0024-swap-nodes-in-pairs/0024-swap-nodes-in-pairs.cpp
+++ 0024-swap-nodes-in-pairs/0024-swap-nodes-in-pairs.cpp
@@ -8,66 +8,24 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
+
 class Solution {
 public:
     ListNode* swapPairs(ListNode* head) {
-        
-        // Base condition to return
-        if ((head == NULL) || (head->next == NULL))
+
+        // Base case
+        if (head == NULL || head->next == NULL)
             return head;
 
-        ListNode* curr  = head;
-        ListNode* prev  = NULL;
+        ListNode* first = head;
+        ListNode* second = head->next;
 
-        // Since we know 2nd element will be first one later on
-        head = head->next;
+        // Recursively swap remaining list
+        first->next = swapPairs(second->next);
 
-        while((curr != NULL) && (curr->next != NULL))
-        {
-            // Selecting first two element which need to be swapped
-            ListNode* n1 = curr;
-            ListNode* n2 = curr->next;
-            
-            // Just incrementing for next iteration
-            curr = n2->next;
+        // Swap current pair
+        second->next = first;
 
-            // Swapping the data
-            n1->next = n2->next;
-            n2->next = n1;
-
-            // connecting logic
-            if (prev != NULL)
-            {
-                prev->next = n2;
-            }
-
-            // Incrementing the prev
-            prev = n1;
-
-        }
-
-        return head;
+        return second;
     }
 };
-/*
-dummy = 0
-
-ListNode* prev  = dummy;
-ListNode* first = head; 
-
-while (prev->next != NULL and first->next-> != NULL)
-
-first  = prev->next;
-second = first->next;
-
-second->next = first;
-
-first->next  = second->next;
-
-prev->next   = second;
-
-prev = first;
-
-return dummy->next;*/
-
- 
