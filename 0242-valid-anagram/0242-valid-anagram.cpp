66e2b0bb42c0b6e817a9f7c772160838fa274082
Abhinav Pandey <56484423+AbhinavSaksham@users.noreply.github.com>
2026-05-18 23:30:44 +0530

    Time: 0 ms (100%), Space: 9.9 MB (10.57%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+
+        if (s.size() != t.size())
+            return false;
+
+        unordered_map<char, int> mp;
+
+        // store frequency of s
+        for (int i = 0; i < s.size(); i++) {
+            mp[s[i]]++;
+        }
+
+        // remove frequency using t
+        for (int i = 0; i < t.size(); i++) {
+
+            mp[t[i]]--;
+
+            if (mp[t[i]] < 0)
+                return false;
+        }
+
+        return true;
+    }
+};
