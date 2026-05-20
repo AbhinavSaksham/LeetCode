658e241ac0c5fd3556e3cfeecc70a51ae9b517c1
Abhinav Pandey <56484423+AbhinavSaksham@users.noreply.github.com>
2026-05-20 20:56:49 +0530

    Time: 0 ms (100%), Space: 8.8 MB (88.09%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.cpp
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -1,23 +1,23 @@
 class Solution {
 public:
     bool isValid(string s) {
-        stack<char> st;
+        stack<char> st;  //because lifo
         
-        for (char c : s) {
-            if (c == '(' || c == '{' || c == '[') {
-                st.push(c);
+        for (char c : s) { //for loop for st
+            if (c == '(' || c == '{' || c == '[') { // if no opening return false
+                st.push(c); //push all opening bracket to stack 
             } else {
-                if (st.empty()) return false;
+                if (st.empty()) return false; 
                 
-                char top = st.top();
-                st.pop();
+                char top = st.top(); //get top element of the stack 
+                st.pop(); //remove top element now 
                 
-                if (c == ')' && top != '(') return false;
+                if (c == ')' && top != '(') return false;  //close is there but not open
                 if (c == '}' && top != '{') return false;
                 if (c == ']' && top != '[') return false;
             }
         }
         
-        return st.empty();
+        return st.empty(); // if st become empty return true
     }
 };
