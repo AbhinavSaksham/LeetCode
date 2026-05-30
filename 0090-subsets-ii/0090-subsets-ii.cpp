afdf06c8d5d3a12c81e475270e2f35b6e8817b1b
Abhinav Pandey <56484423+AbhinavSaksham@users.noreply.github.com>
2026-05-31 01:08:55 +0530

    Time: 3 ms (32.12%), Space: 10.9 MB (24.08%) - LeetHub

---
+++ 0090-subsets-ii/0090-subsets-ii.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    vector<vector<int>> subsetsWithDup(vector<int>& nums) {
+
+        sort(nums.begin(), nums.end()); //sorting 
+        vector<int> temp; // temp arr
+        solve(0, nums, temp); // fn
+        return ans; // solu
+    }
+
+    vector<vector<int>> ans; // final array
+
+    void solve(int index, vector<int>& nums, vector<int>& temp) {
+
+        ans.push_back(temp);
+
+        if(index == nums.size()){
+            return;
+        }
+
+        for (int i = index; i < nums.size(); i++) {
+
+            // Skip duplicates
+            if (i > index && nums[i] == nums[i - 1]) {
+                continue;
+            }
+
+            temp.push_back(nums[i]);
+            solve(i + 1, nums, temp);
+            temp.pop_back();
+        }
+    }
+};
