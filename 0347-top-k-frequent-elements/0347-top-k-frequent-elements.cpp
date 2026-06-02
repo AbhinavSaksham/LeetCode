66e32a8a243724b331fe4b09c253ef23dcaa2876
Abhinav Pandey <56484423+AbhinavSaksham@users.noreply.github.com>
2026-06-02 16:55:21 +0530

    Time: 6 ms (20.13%), Space: 18.8 MB (25.68%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
@@ -4,25 +4,30 @@ public:
 
         unordered_map<int, int> freq;
 
-        // Count frequency of each element
-        for (int num : nums) {
-            freq[num]++;
+        // Count frequency
+        for (int i = 0; i < nums.size(); i++) {
+
+            freq[nums[i]]++;
         }
 
-        // Store {frequency, element}
         vector<pair<int, int>> vec;
 
-        for (auto it : freq) {
-            vec.push_back({it.second, it.first});
+        // Store {frequency, element}
+        for (unordered_map<int, int>::iterator it = freq.begin();
+             it != freq.end();
+             it++) {
+
+            vec.push_back({it->second, it->first});
         }
 
-        // Sort in descending order of frequency
+        // Sort by frequency
         sort(vec.rbegin(), vec.rend());
 
         vector<int> ans;
 
-        // Take first k elements
+        // First k frequent elements
         for (int i = 0; i < k; i++) {
+
             ans.push_back(vec[i].second);
         }
 
