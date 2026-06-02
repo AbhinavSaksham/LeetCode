ea216f496ec71c2c98cd95866e2ccbd8fa2d9844
Abhinav Pandey <56484423+AbhinavSaksham@users.noreply.github.com>
2026-06-03 00:42:28 +0530

    Time: 2 ms (57.37%), Space: 18.1 MB (39.68%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
@@ -4,33 +4,63 @@ public:
 
         unordered_map<int, int> freq;
 
-        // Count frequency
         for (int i = 0; i < nums.size(); i++) {
-
             freq[nums[i]]++;
         }
 
-        vector<pair<int, int>> vec;
+        priority_queue<pair<int, int>> pq;
 
-        // Store {frequency, element}
         for (unordered_map<int, int>::iterator it = freq.begin();
              it != freq.end();
              it++) {
 
-            vec.push_back({it->second, it->first});
+            pq.push({it->second, it->first});
         }
-
-        // Sort by frequency
-        sort(vec.rbegin(), vec.rend());
 
         vector<int> ans;
 
-        // First k frequent elements
         for (int i = 0; i < k; i++) {
 
-            ans.push_back(vec[i].second);
+            ans.push_back(pq.top().second);
+            pq.pop();
         }
 
         return ans;
     }
 };
+
+// class Solution {
+// public:
+//     vector<int> topKFrequent(vector<int>& nums, int k) {
+
+//         unordered_map<int, int> freq;
+
+//         // Count frequency
+//         for (int i = 0; i < nums.size(); i++) {
+//             freq[nums[i]]++;
+//         }
+
+//         vector<pair<int, int>> vec;
+
+//         // Store {frequency, element}
+//         for (unordered_map<int, int>::iterator it = freq.begin();
+//              it != freq.end();
+//              it++) {
+
+//             vec.push_back({it->second, it->first});
+//         }
+
+//         // Sort by frequency
+//         sort(vec.rbegin(), vec.rend());
+
+//         vector<int> ans;
+
+//         // First k frequent elements
+//         for (int i = 0; i < k; i++) {
+
+//             ans.push_back(vec[i].second);
+//         }
+
+//         return ans;
+//     }
+// };
