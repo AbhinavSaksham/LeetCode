9b6d3755f59e87c649ca5c916fec2795eeef9b69
Abhinav Pandey <56484423+AbhinavSaksham@users.noreply.github.com>
2026-06-04 23:20:49 +0530

    Time: 23 ms (29.35%), Space: 42.2 MB (5.29%) - LeetHub

--- 0658-find-k-closest-elements/0658-find-k-closest-elements.cpp
+++ 0658-find-k-closest-elements/0658-find-k-closest-elements.cpp
@@ -3,22 +3,16 @@ public:
     vector<int> findClosestElements(vector<int>& arr, int k, int x) {
 
         //priority_queue<pair<int,int>, greater<int>> pq;
-        //priority_queue<pair<int,int>, vector<pair<int,int>>, greater<pair<int,int>>> pq;
-        priority_queue<pair<int,int>> pq;
+        priority_queue<pair<int,int>, vector<pair<int,int>>, greater<pair<int,int>>> pq;
+        //priority_queue<pair<int,int>> pq;
 
-        for (int i = 0; i < arr.size(); i++)
-        {
-            
+        for (int i = 0; i < arr.size(); i++) {
             pq.push({abs(arr[i] - x), arr[i]});
-
-            if (pq.size() > k){
-            pq.pop();  // 1,2,3,4 ||| 5
-            }
         }
 
         vector<int> ans;
 
-        while(!pq.empty()){
+        for (int i = 0; i < k; i++){
             ans.push_back(pq.top().second);
             pq.pop();
         }
@@ -28,6 +22,38 @@ public:
         return ans;
     }
 };
+
+
+// class Solution {
+// public:
+//     vector<int> findClosestElements(vector<int>& arr, int k, int x) {
+
+//         //priority_queue<pair<int,int>, greater<int>> pq;
+//         //priority_queue<pair<int,int>, vector<pair<int,int>>, greater<pair<int,int>>> pq;
+//         priority_queue<pair<int,int>> pq;
+
+//         for (int i = 0; i < arr.size(); i++)
+//         {
+
+//             pq.push({abs(arr[i] - x), arr[i]});
+
+//             if (pq.size() > k){
+//             pq.pop();  // 1,2,3,4 ||| 5
+//             }
+//         }
+
+//         vector<int> ans;
+
+//         while(!pq.empty()){
+//             ans.push_back(pq.top().second);
+//             pq.pop();
+//         }
+
+//         sort(ans.begin(), ans.end());
+
+//         return ans;
+//     }
+// };
 
 //[3-1, 3-2, 3-3, 3-4, 3-5, 3-6]
 //[2,1,0,1,2, 3]
