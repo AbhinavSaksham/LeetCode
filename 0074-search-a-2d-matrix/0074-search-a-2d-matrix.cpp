5f1cbaf6ec7a2c0748f6398ff234f26a9eb3816d
Abhinav Pandey <56484423+AbhinavSaksham@users.noreply.github.com>
2026-06-05 12:22:45 +0530

    Time: 0 ms (100%), Space: 13.4 MB (43.79%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
@@ -6,24 +6,56 @@ public:
         int col = matrix[0].size();
 
         int start = 0;
-        int end = row * col - 1;
+        int end = row * col -1;
 
-        while (start <= end) { //base case
+        while (start <= end){
+            int mid =  start + (end-start)/2;
 
-            int mid = start + (end - start) / 2; //mid
-
-            int element = matrix[mid / col][mid % col]; //matrix[row][column]
+            int element = matrix[mid / col][mid % col];
 
             if (element == target)
                 return true;
-
-            else if (element < target)
-                start = mid + 1;
-
-            else
-                end = mid - 1;
+            
+            else if (element < target )
+                start = mid +1;
+            
+            else end = mid -1;
         }
-
         return false;
     }
 };
+
+// class Solution {
+// public:
+//     bool searchMatrix(vector<vector<int>>& matrix, int target) {
+
+//         int row = matrix.size();
+//         int col = matrix[0].size();
+
+//         int start = 0;
+//         int end = row * col - 1;
+
+//         while (start <= end) { //base case
+
+//             int mid = start + (end - start) / 2; //mid
+
+//             int element = matrix[mid / col][mid % col]; //matrix[row][column]
+
+//             //cout << "mid = " << mid << endl;
+//             //cout << "rowIndex = " << rowIndex << endl;
+//             //cout << "colIndex = " << colIndex << endl;
+//             //cout << "element = " << element << endl;
+
+//             if (element == target)
+//                 return true;
+
+//             else if (element < target)
+//                 start = mid + 1;
+
+//             else
+//                 end = mid - 1;
+//         }
+
+//         return false;
+//     }
+// };
