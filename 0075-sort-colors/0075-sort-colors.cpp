82cf710ed49dadb2415fa37caaec08ab65d4d12f
Abhinav Pandey <56484423+AbhinavSaksham@users.noreply.github.com>
2026-06-17 11:21:41 +0530

    Time: 0 ms (100%), Space: 11.8 MB (11.16%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.cpp
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -2,6 +2,27 @@ class Solution {
 public:
     void sortColors(vector<int>& nums) {
 
-        sort(nums.begin(), nums.end()); //sort in asc.
+       // sort(nums.begin(), nums.end()); //sort in asc.
+       vector <int>ans;
+
+       for (int i = 0; i < nums.size(); i++)
+       {
+        if (nums[i] == 0){
+            ans.push_back(nums[i]);
+        }
+       }
+       for (int i = 0; i < nums.size(); i++)
+            {
+            if (nums[i] == 1){
+            ans.push_back(nums[i]);
+        }
+       }
+       for (int i = 0; i < nums.size(); i++)
+            {
+            if (nums[i] == 2){
+            ans.push_back(nums[i]);
+        }
+       }
+       nums = ans;
     }
 };
