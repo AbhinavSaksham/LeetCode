c6a5812874ae23808a688304ebe93641db8912e8
Abhinav Pandey <56484423+AbhinavSaksham@users.noreply.github.com>
2026-07-04 19:05:23 +0530

    Time: 0 ms (100%), Space: 71.6 MB (80.83%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.cpp
+++ 0053-maximum-subarray/0053-maximum-subarray.cpp
@@ -3,38 +3,63 @@ public:
     int maxSubArray(vector<int>& nums) {
 
         int sum = 0;
-        int maxi = INT_MIN;
-
-        int start = 0;      // Current subarray start
-        int ansStart = 0;   // Answer start index
-        int ansEnd = 0;     // Answer end index
+        int maxi = INT_MIN;   // Store maximum subarray sum
 
         for (int i = 0; i < nums.size(); i++) {
 
-            // Start a new subarray
-            if (sum == 0) {
-                start = i;
-            }
+            // Add current element
+            sum += nums[i];
 
-            sum += nums[i]; // Add current element
+            // Update maximum sum
+            maxi = max(maxi, sum);
 
-            if (sum > maxi) {  // Update maximum sum
-                maxi = sum;
-                ansStart = start;
-                ansEnd = i;
-            }
-
-            if (sum < 0) { // Reset if sum becomes negative
+            // If sum becomes negative, start a new subarray
+            if (sum < 0) {
                 sum = 0;
             }
         }
 
-        // Optional: print the subarray
-        /*
-        for (int i = ansStart; i <= ansEnd; i++) {
-            cout << nums[i] << " ";
-        }
-        */
         return maxi;
     }
 };
+
+// class Solution {
+// public:
+//     int maxSubArray(vector<int>& nums) {
+
+//         int sum = 0;
+//         int maxi = INT_MIN;
+
+//         int start = 0;      // Current subarray start
+//         int ansStart = 0;   // Answer start index
+//         int ansEnd = 0;     // Answer end index
+
+//         for (int i = 0; i < nums.size(); i++) {
+
+//             // Start a new subarray
+//             if (sum == 0) {
+//                 start = i;
+//             }
+
+//             sum += nums[i]; // Add current element
+
+//             if (sum > maxi) {  // Update maximum sum
+//                 maxi = sum;
+//                 ansStart = start;
+//                 ansEnd = i;
+//             }
+
+//             if (sum < 0) { // Reset if sum becomes negative
+//                 sum = 0;
+//             }
+//         }
+
+//         // Optional: print the subarray
+//         /*
+//         for (int i = ansStart; i <= ansEnd; i++) {
+//             cout << nums[i] << " ";
+//         }
+//         */
+//         return maxi;
+//     }
+// };
