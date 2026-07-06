3be7575963b9ccae8074f81c81611cf3c5f1f06a
Abhinav Pandey <56484423+AbhinavSaksham@users.noreply.github.com>
2026-07-06 23:55:53 +0530

    Time: 8 ms (15.1%), Space: 15.2 MB (30.6%) - LeetHub

---
+++ 1288-remove-covered-intervals/1288-remove-covered-intervals.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+
+    static bool cmp(vector<int>& a, vector<int>& b) {
+
+        // Sort by start
+        if (a[0] != b[0]) {
+            return a[0] < b[0];
+        }
+
+        // If start is same, larger end first
+        return a[1] > b[1];
+    }
+
+    int removeCoveredIntervals(vector<vector<int>>& intervals) {
+
+        sort(intervals.begin(), intervals.end(), cmp); //O(nlog n)
+
+        int count = 1;
+        int maxEnd = intervals[0][1];
+
+        for (int i = 1; i < intervals.size(); i++) {
+
+            // Covered interval
+            if (intervals[i][1] <= maxEnd) {
+                continue;
+            }
+
+            // Not covered
+            count++;
+            maxEnd = intervals[i][1];
+        }
+
+        return count;
+    }
+};
