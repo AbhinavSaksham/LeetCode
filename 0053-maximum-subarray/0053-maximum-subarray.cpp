4e1a255b3cfbe156adccbd98a5f4d74bb3162506
Abhinav Pandey <56484423+AbhinavSaksham@users.noreply.github.com>
2026-07-10 16:52:28 +0530

    Time: 0 ms (100%), Space: 71.7 MB (80.97%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.cpp
+++ 0053-maximum-subarray/0053-maximum-subarray.cpp
@@ -1,65 +1,22 @@
 class Solution {
 public:
     int maxSubArray(vector<int>& nums) {
-
+        
+        int maxi = INT_MIN;
         int sum = 0;
-        int maxi = INT_MIN;   // Store maximum subarray sum
 
-        for (int i = 0; i < nums.size(); i++) {
+        for (int i = 0; i < nums.size(); i++){
 
-            // Add current element
             sum += nums[i];
-
-            // Update maximum sum
             maxi = max(maxi, sum);
 
-            // If sum becomes negative, start a new subarray
-            if (sum < 0) {
+            if(sum < 0){
                 sum = 0;
             }
-        }
 
+        
+        }
         return maxi;
+
     }
 };
-
-// class Solution {
-// public:
-//     int maxSubArray(vector<int>& nums) {
-
-//         int sum = 0;
-//         int maxi = INT_MIN;
-
-//         int start = 0;      // Current subarray start
-//         int ansStart = 0;   // Answer start index
-//         int ansEnd = 0;     // Answer end index
-
-//         for (int i = 0; i < nums.size(); i++) {
-
-//             // Start a new subarray
-//             if (sum == 0) {
-//                 start = i;
-//             }
-
-//             sum += nums[i]; // Add current element
-
-//             if (sum > maxi) {  // Update maximum sum
-//                 maxi = sum;
-//                 ansStart = start;
-//                 ansEnd = i;
-//             }
-
-//             if (sum < 0) { // Reset if sum becomes negative
-//                 sum = 0;
-//             }
-//         }
-
-//         // Optional: print the subarray
-//         /*
-//         for (int i = ansStart; i <= ansEnd; i++) {
-//             cout << nums[i] << " ";
-//         }
-//         */
-//         return maxi;
-//     }
-// };
