71f4909b9e93ecf00055213ca10bd75ddd2a79df
Abhinav Pandey <56484423+AbhinavSaksham@users.noreply.github.com>
2026-07-12 15:09:42 +0530

    Time: 0 ms (100%), Space: 9 MB (38.18%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.cpp
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -6,15 +6,20 @@ public:
         for (char c : s) { //for loop for st
             if (c == '(' || c == '{' || c == '[') { // if no opening return false
                 st.push(c); //push all opening bracket to stack 
-            } else {
-                if (st.empty()) return false; 
+            } 
+            else {
+                if (st.empty()) {
+                    return false;
+                } 
                 
-                char top = st.top(); //get top element of the stack 
-                st.pop(); //remove top element now 
+                else {
+                    char top = st.top(); //get top element of the stack 
+                    st.pop(); //remove top element now 
                 
-                if (c == ')' && top != '(') return false;  //close is there but not open
-                if (c == '}' && top != '{') return false;
-                if (c == ']' && top != '[') return false;
+                    if (c == ')' && top != '(') return false;  //close is there but not open
+                    if (c == '}' && top != '{') return false;
+                    if (c == ']' && top != '[') return false;
+                }
             }
         }
         
