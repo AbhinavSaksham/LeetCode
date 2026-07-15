1872849d3eeb9f5799876170a7bf3e6c9e12f58f
Abhinav Pandey <56484423+AbhinavSaksham@users.noreply.github.com>
2026-07-15 15:53:04 +0530

    Time: 3 ms (88.9%), Space: 24 MB (30.69%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.cpp
+++ 0056-merge-intervals/0056-merge-intervals.cpp
@@ -1,37 +1,65 @@
 class Solution {
 public:
     vector<vector<int>> merge(vector<vector<int>>& intervals) {
-
         vector<vector<int>> ans;
 
-        // Sort intervals by starting point
-        sort(intervals.begin(), intervals.end());
-
-        // Put first interval in answer
+        //int i = 0;
+        //int n = intervals.size();
+        sort(intervals.begin(), intervals.end()); //nlogn
         ans.push_back(intervals[0]);
 
-        for(int i = 1; i < intervals.size(); i++) {
+       for (int i = 1; i < intervals.size(); i++) { //0(n)
 
-            // Current interval
-            int start = intervals[i][0];
-            int end = intervals[i][1];
-
-            // Last interval in answer
-            int lastEnd = ans.back()[1];
-
-            // Overlapping intervals
-            if(start <= lastEnd) {
-
-                ans.back()[1] =
-                max(lastEnd, end);
+            // Overlap
+            if (intervals[i][0] <= ans.back()[1]) {
+                ans.back()[1] = max(ans.back()[1], intervals[i][1]);
             }
+
+            // No overlap
             else {
 
-                // Non-overlapping
                 ans.push_back(intervals[i]);
             }
-        }
-
-        return ans;
+       }
+       return ans;
     }
 };
+
+
+// class Solution {
+// public:
+//     vector<vector<int>> merge(vector<vector<int>>& intervals) {
+
+//         vector<vector<int>> ans;
+
+//         // Sort intervals by starting point
+//         sort(intervals.begin(), intervals.end());
+
+//         // Put first interval in answer
+//         ans.push_back(intervals[0]);
+
+//         for(int i = 1; i < intervals.size(); i++) {
+
+//             // Current interval
+//             int start = intervals[i][0];
+//             int end = intervals[i][1];
+
+//             // Last interval in answer
+//             int lastEnd = ans.back()[1];
+
+//             // Overlapping intervals
+//             if(start <= lastEnd) {
+
+//                 ans.back()[1] =
+//                 max(lastEnd, end);
+//             }
+//             else {
+
+//                 // Non-overlapping
+//                 ans.push_back(intervals[i]);
+//             }
+//         }
+
+//         return ans;
+//     }
+// };
