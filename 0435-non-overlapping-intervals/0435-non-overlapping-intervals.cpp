ce747b6fec482a0c6cce7a201b2b80ffef4f39b0
Abhinav Pandey <56484423+AbhinavSaksham@users.noreply.github.com>
2026-07-15 18:44:15 +0530

    Time: 68 ms (19.21%), Space: 94 MB (68.85%) - LeetHub

--- 0435-non-overlapping-intervals/0435-non-overlapping-intervals.cpp
+++ 0435-non-overlapping-intervals/0435-non-overlapping-intervals.cpp
@@ -5,26 +5,26 @@ public:
         sort(intervals.begin(), intervals.end());
 
         int ans = 0;
-        int prevEnd = intervals[0][1];
+        int prevEnd = intervals[0][1]; //2
 
         for (int i = 1; i < intervals.size(); i++) {
 
             // Overlap
-            if (intervals[i][0] < prevEnd) {
+            if (intervals[i][0] < prevEnd) { //2 <  2, 3<3, 1< 4
 
-                ans++;
+                ans++; //1
 
                 // Keep interval with smaller end
-                prevEnd = min(prevEnd, intervals[i][1]);
+                prevEnd = min(prevEnd, intervals[i][1]); //1
             }
 
             // No overlap
             else {
 
-                prevEnd = intervals[i][1];
+                prevEnd = intervals[i][1]; //3 , 4 
             }
         }
 
-        return ans;
+        return ans; //1
     }
 };
