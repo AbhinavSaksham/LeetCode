4f15d1905df3b6249212fc752d68950537905402
Abhinav Pandey <56484423+AbhinavSaksham@users.noreply.github.com>
2026-07-25 00:01:41 +0530

    Time: 0 ms (100%), Space: 10.6 MB (46.95%) - LeetHub

---
+++ 0198-house-robber/0198-house-robber.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int rob(vector<int>& nums) {
+
+        int n = nums.size();
+
+        // Base cases
+        if(n == 1) {
+            return nums[0];
+        }
+
+        vector<int> dp(n);
+
+        dp[0] = nums[0];
+        dp[1] = max(nums[0], nums[1]);
+
+        // Build DP array
+        for(int i = 2; i < n; i++) {
+
+            // Either skip current house
+            // or rob current house
+            dp[i] = max(dp[i - 1],
+                        dp[i - 2] + nums[i]);
+        }
+
+        return dp[n - 1];
+    }
+};
