9550e4b2d00824108fc01169dfa588922e4f7f63
Abhinav Pandey <56484423+AbhinavSaksham@users.noreply.github.com>
2026-08-08 09:29:15 +0530

    Time: 0 ms (100%), Space: 22 MB (53.39%) - LeetHub

--- 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.cpp
+++ 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.cpp
@@ -1,30 +1,19 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) 
- *         : val(x), left(left), right(right) {}
- * };
- */
-
 class Solution {
 public:
     int maxDepth(TreeNode* root) {
 
-        if (root == NULL)
+        // Base case
+        if (root == NULL) {
             return 0;
+        }
 
-        int heightleft = maxDepth(root->left); //left
-        int heightright = maxDepth(root->right); //right
+        // Find depth of left and right subtree
+        int ansL = maxDepth(root->left);
+        int ansR = maxDepth(root->right);
 
-        if (heightright > heightleft)
-            return heightright + 1;
+        // Current node adds 1
+        int ans = 1 + max(ansL, ansR); //+1 because of root node
 
-        else
-            return heightleft +1;
+        return ans;
     }
 };
