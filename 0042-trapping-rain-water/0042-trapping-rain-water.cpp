31622d804c839da67e8da4a59f7e99a5f1130103
Abhinav Pandey <56484423+AbhinavSaksham@users.noreply.github.com>
2026-08-12 00:27:34 +0530

    Time: 0 ms (100%), Space: 25.9 MB (78.05%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.cpp
+++ 0042-trapping-rain-water/0042-trapping-rain-water.cpp
@@ -2,42 +2,33 @@ class Solution {
 public:
     int trap(vector<int>& height) {
 
-        // Two pointers: one from left and one from right
         int l = 0;
         int r = height.size() - 1;
 
-        // Maximum height seen from left and right
         int leftMax = 0;
         int rightMax = 0;
 
         int ans = 0;
 
-        while (l <= r) {
+        while (l < r) {
 
-            // Process the side with smaller height
+            // Left side is smaller, so calculate water using leftMax
             if (height[l] <= height[r]) {
 
-                // Update left maximum
-                if (height[l] >= leftMax) {
+                if (height[l] >= leftMax)
                     leftMax = height[l];
-                }
-                else {
-                    // Water trapped = leftMax - current height
+                else
                     ans += leftMax - height[l];
-                }
 
                 l++;
             }
             else {
 
-                // Update right maximum
-                if (height[r] >= rightMax) {
+                // Right side is smaller, so calculate water using rightMax
+                if (height[r] >= rightMax)
                     rightMax = height[r];
-                }
-                else {
-                    // Water trapped = rightMax - current height
+                else
                     ans += rightMax - height[r];
-                }
 
                 r--;
             }
