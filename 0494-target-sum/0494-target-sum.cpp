28966c781dbb3cb4b3ec2e2e211638d884ed7eed
Abhinav Pandey <56484423+AbhinavSaksham@users.noreply.github.com>
2026-08-18 00:08:52 +0530

    Time: 806 ms (13.83%), Space: 11.5 MB (98.1%) - LeetHub

---
+++ 0494-target-sum/0494-target-sum.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int findTargetSumWays(vector<int>& nums, int target) {
+
+        solve(nums, 0, 0, target);
+        return count;
+    }
+
+private:
+
+    int count = 0;
+    void solve(vector<int>& nums, int index, int sum, int target) {
+
+        // All numbers have been used
+        if(index == nums.size()) {
+
+            // Found one valid expression
+            if(sum == target) {
+                count++;
+            }
+            return;
+        }
+        // Choose '+'
+        solve(nums, index + 1, sum + nums[index], target);
+        // Choose '-'
+        solve(nums, index + 1, sum - nums[index], target);
+    }
+};
