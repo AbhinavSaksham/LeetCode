cabe439f5b176b0ff54798e9ce574e6e081cc5d3
Abhinav Pandey <56484423+AbhinavSaksham@users.noreply.github.com>
2026-09-02 00:58:31 +0530

    Time: 0 ms (100%), Space: 8.6 MB (63.91%) - LeetHub

--- 0091-decode-ways/0091-decode-ways.cpp
+++ 0091-decode-ways/0091-decode-ways.cpp
@@ -3,15 +3,9 @@ public:
     int numDecodings(string s) {
 
         int n = s.size();
-
-        // dp[i] = number of ways to decode first i characters
-        vector<int> dp(n + 1, 0);
-
-        // Empty string has one way
-        dp[0] = 1;
-
-        // First character cannot be 0
-        dp[1] = (s[0] != '0');
+        vector<int> dp(n + 1, 0);   // dp[i] = number of ways to decode first i characters
+        dp[0] = 1;                  // Empty string has only one way 
+        dp[1] = (s[0] != '0');      // First character cannot be 0
 
         for(int i = 2; i <= n; i++) {
 
